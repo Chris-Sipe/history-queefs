833343746920ac77237faeae298c42a5c7a0f63d
Chris Sipe <chrissipe12@gmail.com>
2023-05-20 01:27:38 -0400

    import vids

--- premiere-script/merge-videos.jsx
+++ premiere-script/merge-videos.jsx
@@ -2,31 +2,35 @@
 // var app = new PremierePro();
 var project = app.project;
 var seq;
-var outputFilePath =  "C:\Users\chris\OneDrive\Documents\history-queefs\history-queefs\premiere-script\output-file.mp4";
+var outputFilePath = "~/Documents/history-queefs/history-queefs/premiere-script";
 var filesFolderPath = "~/Documents/history-queefs/history-queefs/premiere-script/input-vids";
-var insertionBin = project.getInsertionBin();
 
 // Get the video files in the folder
 var filesFolder = new Folder(filesFolderPath);
 var files = filesFolder.getFiles("*.mp4");
 var filePaths = [];
 for (var i = 0; i < files.length; i++) {
-    filePaths.push(files[i].fullName);
+    var filePath = addExtraBackslash(files[i].fsName);
+    filePaths.push(filePath);
 }
 
-// Create a new sequence with the settings of the first video file
-var importSuccess = project.importFiles(filePaths, false, insertionBin, false);
+// import video files into project
+var importSuccess = project.importFiles(filePaths);
 
-var firstSeq = project.createNewSequenceFromClips("Sequence 1", [firstVideo[0]], null);
-seq = firstSeq;
+// var firstSeq = project.createNewSequenceFromClips("Sequence 1", [firstVideo[0]], null);
+// seq = firstSeq;
 
-// Add the rest of the videos to the sequence
-for (var i = 1; i < files.length; i++) {
-    var video = project.importFiles([files[i]]);
-    seq.insertClip(video[0], seq.getOutPoint());
+// // Add the rest of the videos to the sequence
+// for (var i = 1; i < files.length; i++) {
+//     var video = project.importFiles([files[i]]);
+//     seq.insertClip(video[0], seq.getOutPoint());
+// }
+
+// // Export the sequence as an MP4 file
+// var exportSettings = app.encoder.createExportOptions(outputFilePath, 1080, 1920, app.encoder.ENCODE_QUALITY_HIGH);
+// exportSettings.setPresetFormat("MP4");
+// app.encoder.exportSequence(seq, outputFilePath, exportSettings);
+
+function addExtraBackslash(str) {
+    return str.replace(/\\/g, "\\\\");
 }
-
-// Export the sequence as an MP4 file
-var exportSettings = app.encoder.createExportOptions(outputFilePath, 1080, 1920, app.encoder.ENCODE_QUALITY_HIGH);
-exportSettings.setPresetFormat("MP4");
-app.encoder.exportSequence(seq, outputFilePath, exportSettings);
