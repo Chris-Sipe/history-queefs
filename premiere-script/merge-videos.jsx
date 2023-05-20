c82726e5aa6c57f1a18b7db38ab4673cbfa69208
Chris Sipe <chrissipe12@gmail.com>
2023-05-20 02:10:10 -0400

    adding clips to sequence

--- premiere-script/merge-videos.jsx
+++ premiere-script/merge-videos.jsx
@@ -1,11 +1,8 @@
-// Import the necessary classes from the Premiere Pro scripting API
-// var app = new PremierePro();
 var project = app.project;
-var seq;
 var outputFilePath = "~/Documents/history-queefs/history-queefs/premiere-script";
 var filesFolderPath = "~/Documents/history-queefs/history-queefs/premiere-script/input-vids";
 
-// Get the video files in the folder
+// import video files into project
 var filesFolder = new Folder(filesFolderPath);
 var files = filesFolder.getFiles("*.mp4");
 var filePaths = [];
@@ -13,18 +10,15 @@ for (var i = 0; i < files.length; i++) {
     var filePath = addExtraBackslash(files[i].fsName);
     filePaths.push(filePath);
 }
-
-// import video files into project
 var importSuccess = project.importFiles(filePaths);
 
-// var firstSeq = project.createNewSequenceFromClips("Sequence 1", [firstVideo[0]], null);
-// seq = firstSeq;
-
-// // Add the rest of the videos to the sequence
-// for (var i = 1; i < files.length; i++) {
-//     var video = project.importFiles([files[i]]);
-//     seq.insertClip(video[0], seq.getOutPoint());
-// }
+// insert clips into sequence
+var availableClipsCollection = project.rootItem.children;
+var availableClipsArray = [];
+for (var i = 0; i < availableClipsCollection.length; i++) {
+    availableClipsArray.push(availableClipsCollection[i])
+}
+var firstSeq = project.createNewSequenceFromClips("sequence 1", availableClipsArray, null);
 
 // // Export the sequence as an MP4 file
 // var exportSettings = app.encoder.createExportOptions(outputFilePath, 1080, 1920, app.encoder.ENCODE_QUALITY_HIGH);
